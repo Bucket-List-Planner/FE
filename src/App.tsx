bc0db36b5b8e35161ee0c91eb8f719f05520001e
Aleksandra Foksman <aleksandrafoksman@aleksandras-mbp.home>
2020-04-30 13:56:01 -0700

    config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, {FunctionComponent} from 'react'
+
+let App: FunctionComponent = () => {
+    let pi:number = 3
+    return(
+        <div>
+            Hello {pi}
+        </div>
+    )
+}
+
+
+export default App
