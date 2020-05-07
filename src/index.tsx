ff98902d7caec7c142e83898b7a78fad06d94800
Aleksandra Foksman <aleksandrafoksman@aleksandras-mbp.home>
2020-05-07 15:45:43 -0700

    btn mood complited

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,24 @@ import GlobalStyles from './globalStyles'
 
 
 const theme = {
-    primary: '#594DFF',
-    secondary: '#54DEFD',
-    contrast: "#EB4963",
+    primary: '#8941FF', // purple
+    primaryDark: '#594DFF',
+    secondary: '#54DEFD', // blue
+    secondaryDark: '#1FACE9',
+    contrast: "#FAFF00", // yellow
+    contrastDark: '#FFC700',
+
     colors: {
-        powderWhite: "#FFFDF9",
+        danger: '#FC5050',
+        dangerDark: '#EA1515',
+        warning: '#FDCA17',
+        warningDark: '#FE9800',
+        success: '#5CD219',
+        successDark: '#4B9B1A',
+        info: '#1ECEE5',
+        infoDark: '#24ABBD',
     },
+
     breakpoints: {
         minWidth: 280,
         maxWidth: 1000
